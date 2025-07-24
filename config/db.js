c2e1116ee1436b716612857ddf3e898a5c6c39a7
saifAqeel <saifaqeel2056@gmail.com>
2025-07-25 05:18:11 +0530

    Resolve merge conflicts

---
+++ config/db.js
@@ -0,0 +1,16 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config(); // Load environment variables
+
+// const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/mydb";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(mongoURI);
+//     console.log("✅ MongoDB connected successfully");
+//   } catch (err) {
+//     console.error("❌ MongoDB connection error:", err);
+//     process.exit(1); // Exit process on failure
+//   }
+// };
+
+// module.exports = connectDB;
