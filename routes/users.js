1c59d6c014719b897ecd990966c992a60cd88796
saifAqeel <112947964+saifAqeel@users.noreply.github.com>
2025-07-25 03:16:28 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterset");
+mongoose.connect(process.env.MONGO_URI,
+  { userNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 
 const userSchema = mongoose.Schema({
